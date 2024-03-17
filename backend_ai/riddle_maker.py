38062c5eb91183e0d76c8ed51265c5712adbdbb5
spytrac <anujjariwala@arizona.edu>
2024-03-16 17:30:51 -0700

    riddle_maker.py completed

--- backend_ai/riddle_maker.py
+++ backend_ai/riddle_maker.py
@@ -1,5 +1,4 @@
 import os
-
 from groq import Groq
 
 client = Groq(
@@ -8,19 +7,17 @@ client = Groq(
 )
 
 
-userinput = input("Enter your word: ")
-
-
-
-
-chat_completion = client.chat.completions.create(
+def generate_riddle(userinput):
+    riddle = "Generate a riddle for the word {} and just give the riddle as output and nothing else.".format(userinput)
+    
+    chat_completion = client.chat.completions.create(
     messages=[
-        {"role": "system", "content": "you are a helpful assistant."},
+        {"role": "system", "content": "you are a helpful assistant who have a knowledge of riddles."},
         {
             "role": "user",
             "content": "Generate a riddle for the word {} and just give the riddle as output and nothing else.".format(userinput),
         },
     ],
     model="mixtral-8x7b-32768",
-)
-print(chat_completion.choices[0].message.content)
+    )
+    return chat_completion.choices[0].message.content
